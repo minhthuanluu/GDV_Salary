421873c1f40d375cd4b0af7f8b426abba1e655b3
John <minhthuan.luu@gmail.com>
2021-10-28 19:14:34 +0700

    deploy testflight

--- src/adminapi/untils.js
+++ src/adminapi/untils.js
@@ -1,7 +1,8 @@
 import { Platform } from "react-native";
 import { _retrieveData } from "../utils/Storage";
 
-const protocol = Platform.OS == "android" ? "http://" : "https://";
+// const protocol = Platform.OS == "android" ? "http://" : "https://";
+const protocol = "http://"
 export const baseUrl = `${protocol}hochiminh.mobifone.vn/luongGDV/api/`;
 export const imgUrl = `${protocol}hochiminh.mobifone.vn/luongGDV/`;
 
